5b5d439838817d1bf619c0eea783e3d8c2fd7ae9
Morgan Gilbert <morgan.el.gilbert@gmail.com>
2022-09-08 22:37:36 -0400

    added some css, cleaned code, adjustments to html to make cleaner viewing.

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -3,22 +3,22 @@ const { Post } = require('../models');
 const postdata = [
     {
       title: 'Donec posuere metus vitae ipsum.',
-      content: 'information',
+      content: 'Lorem ipsum dolor sit amet, feugait elaboraret eu per, ut qui mazim abhorreant expetendis, vix impetus definitiones te. Vis iusto choro sensibus id, cum case tacimates maluisset cu. Magna consulatu duo an. Decore necessitatibus no mea. Ut nec deleniti iudicabit, libris maiestatis cu his, at eos nihil splendide.',
       user_id: 1
     },
     {
       title: 'Morbi non quam nec dui luctus rutrum.',
-      content: 'information',
+      content: 'Porro fuisset cotidieque ne est, eam ne stet dolorum perpetua. Pri omnium definiebas neglegentur cu, et sea dolorem petentium urbanitas. Ne cum cetero persius recusabo, ea vim purto animal alienum.',
       user_id: 2
     },
     {
       title: 'Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue.',
-      content: 'information',
+      content: 'Ne has fastidii deseruisse interesset, ei oratio praesent tractatos eum. Habemus principes disputationi ea eum, eu est timeam veritus consetetur, ius cu posse prompta. Legere nonumes forensibus et vim, sit no accumsan salutatus. An sint accusam qui, no mel unum dissentiunt theophrastus, te his aperiam scriptorem.',
       user_id: 3
     },
     {
       title: 'Nunc purus.',
-      content: 'information',
+      content: 'Ei affert accusata tincidunt vis. Sed ea augue reprimique. At eos volumus assentior, at est discere consulatu incorrupte. At erat idque atomorum vix, blandit vivendum necessitatibus sed id, has detraxit dissentiunt an. Duo partiendo percipitur ad, ne justo volutpat explicari vel. Cum at falli nonumy timeam, detraxit quaestio postulant ad sea.',
       user_id: 4
     }
 ];
